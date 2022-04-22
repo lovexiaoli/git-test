411acabb7d08176bd3611b02422b1985ae21601d
cj <cj@c.com>
2022-04-22 15:46:39 +0800

    second commit

--- src/main/java/com/c/git/hello.java
+++ src/main/java/com/c/git/hello.java
@@ -3,5 +3,6 @@ package com.c.git;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello...git");
+        System.out.println("hello...git22222222");
     }
 }
