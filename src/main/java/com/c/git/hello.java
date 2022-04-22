af643f8fa294171aa8c0d7ba70a6de87433dc11d
cj <cj@c.com>
2022-04-22 15:57:08 +0800

    master second  commit

--- src/main/java/com/c/git/hello.java
+++ src/main/java/com/c/git/hello.java
@@ -6,5 +6,6 @@ public class hello {
         System.out.println("hello...git22222222");
         System.out.println("hello...git333333333333333333");
         System.out.println("hello...git44444444444444444444444444444");
+        System.out.println("master test");
     }
 }
